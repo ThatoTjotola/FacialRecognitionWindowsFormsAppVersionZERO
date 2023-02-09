7b6a0cd356d703493b2391ff973b0408bf82a301
Jimmy Tjotola <ST10091865@vcconnect.edu.za>
2023-02-09 14:00:40 +0200

    test

--- FacialRecognitionForm.cs
+++ FacialRecognitionForm.cs
@@ -2,6 +2,8 @@
 using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.Sql;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -17,7 +19,7 @@ namespace FacialRecognitionApp
     {
 
 
-        
+
         //Declare Variables to use them in all this project
         MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
         HaarCascade faceDetected;
@@ -26,27 +28,30 @@ namespace FacialRecognitionApp
         Image<Gray, byte> result;
         Image<Gray, byte> TrainedFace = null;
         Image<Gray, byte> grayFace = null;
-        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        public static List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+
         List<string> labels = new List<string>();
         List<string> Users = new List<string>();
+
         int Count, NumLables, t;
         string name, names = null;
 
-       
+
         public FacialRecognitionForm()
         {
 
             InitializeComponent();
             //HaarCascade is for face detection
             faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
-            
-                string Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
-                string [] Labels = Labelsinf.Split(',');
+
+            String Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+            string[] Labels = Labelsinf.Split(',');
             //The first label before , will be the number of faces saved.
             //Try catch skips for some reason debug and further investigate
             //Fixed 
             try
             {
+
                 NumLables = Convert.ToInt16(Labels[0]);
                 Count = NumLables;
                 string FacesLoad;
@@ -54,22 +59,23 @@ namespace FacialRecognitionApp
                 {
                     FacesLoad = "face" + i + ".bmp";
                     trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/" + FacesLoad));
-                    //trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
                     labels.Add(Labels[i]);
                 }
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show("Database is currently empty");
             }
-            
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
         }
-        
+
         private void button2_Click(object sender, EventArgs e)
         {
-           
-            
+
+
             Count = Count + 1;
             grayFace = camera.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
             MCvAvgComp[][] DetectedFaces = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
@@ -80,17 +86,28 @@ namespace FacialRecognitionApp
             }
             //Where face is first detected and then added into an array 
             TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-            //TrainedFace is added into training images array
-            //if (trainingImages.Contains(TrainedFace))
-            //{
-            //    MessageBox.Show("User Already exists");
-            //}
-            //else
 
+            Random ran = new Random();
             trainingImages.Add(TrainedFace);
-            labels.Add(textBox1.Text); 
+            User.ID = ran.Next(100);
+            User.username = textBox1.Text;
+            User.age = age.Text;
+            User.faculty = faculty.Text;
+
+
+            labels.Add(User.username);
+            Data();
+
+
+
+
+
+
+
+            //labels.Add(age.Text);
+            //labels.Add(faculty.Text);
             File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
-            
+
 
             for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
             {
@@ -100,17 +117,66 @@ namespace FacialRecognitionApp
             }
             MessageBox.Show(textBox1.Text + " Added Successfully");
         }
-    
 
-        
-       
-
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            
-            camera = new Capture();
-            camera.QueryFrame();
-            Application.Idle += new EventHandler(FrameProcedure);
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            age.Text = "";
+            faculty.Text = "";
+
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.AntiqueWhite), 3);
+                //core
+
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.000001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 4000, ref termCriterias);
+                    name = recognizer.Recognize(result);
+
+                    SqlConnection con2 = new SqlConnection("Data Source = awehserverst10091865.database.windows.net; Initial Catalog = FacialRecognitionApp; User ID = jimmy; Password=4731598819Amo");
+                    con2.Open();
+                    //Create getters and setters for og usertxt inorder for not null
+
+                    SqlCommand cmd2 = new SqlCommand("SELECT * FROM userdata where name = '" + name + "'", con2);
+                    
+                    SqlDataReader reader = cmd2.ExecuteReader();
+                    if(reader.Read())
+                    {
+                        label5.Text = reader[0].ToString();
+                        textBox1.Text = reader[1].ToString();
+                        age.Text = reader[2].ToString();
+                        faculty.Text = reader[3].ToString();
+                    }
+
+                    
+
+
+                   
+
+                }
+
+                Users.Add("");
+            }
+
+
+
         }
 
         
@@ -124,21 +190,55 @@ namespace FacialRecognitionApp
             {
                 result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
                 Frame.Draw(f.rect, new Bgr(Color.AntiqueWhite), 3);
-                
+                //core
+
                 if (trainingImages.ToArray().Length != 0)
                 {
-                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.00001);
-                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 3000, ref termCriterias);
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.000001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 20000, ref termCriterias);
                     name = recognizer.Recognize(result);
-                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.DarkBlue));
 
                 }
+                
+
 
                 Users.Add("");
             }
             imageBox1.Image = Frame;
             names = "";
             Users.Clear();
+
+        }
+
+        public static class User
+        {
+            public static Int32 ID { get; set; }
+            public static string username { get; set; } = null;
+            public static string age { get; set; } = null;
+
+            public static string faculty { get; set; }
+
+        }
+        private void Data ()
+        {
+            SqlConnection con = new SqlConnection("Data Source = awehserverst10091865.database.windows.net; Initial Catalog = FacialRecognitionApp; User ID = jimmy; Password=4731598819Amo");
+            
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO userdata VALUES (@ID,@name,@age,@faculty)", con);
+            //Remove as primary key and place other value as primary key too prevent duplicates
+
+            
+            cmd.Parameters.AddWithValue("@ID",User.ID);
+            cmd.Parameters.AddWithValue("@name", User.username);
+            cmd.Parameters.AddWithValue("@age", User.age);
+            cmd.Parameters.AddWithValue("@faculty", User.faculty);
+            cmd.ExecuteNonQuery();
+
+
+            con.Close();
+            MessageBox.Show("Saved", "UserData");
         }
     }
 
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace FacialRecognitionApp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap idea {
+            get {
+                object obj = ResourceManager.GetObject("idea", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
